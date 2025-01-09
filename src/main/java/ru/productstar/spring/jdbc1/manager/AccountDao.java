f8f96333d622d5ca37c0fed58d7b0c3d9123653a
Виктор Орлов <vorlov@topsbi.ru>
2025-01-09 17:14:57 +0300

    '3/6 tests' passed

--- src/main/java/ru/productstar/spring/jdbc1/manager/AccountDao.java
+++ src/main/java/ru/productstar/spring/jdbc1/manager/AccountDao.java
@@ -5,8 +5,6 @@ import java.util.List;
 public interface AccountDao {
     Account addAccount(long id, long amount);
 
-    Account addAccount(long amount);
-
     Account getAccount(long accountId);
 
     void setAmount(long accountId, long amount);
