bec67e63d8ded849b6f53797b5141bbd3fea5ca9
Виктор Орлов <vorlov@topsbi.ru>
2025-01-09 17:15:18 +0300

    Revert "'3/6 tests' passed"
    This reverts commit f8f96333d622d5ca37c0fed58d7b0c3d9123653a.

--- src/main/java/ru/productstar/spring/jdbc1/manager/AccountDao.java
+++ src/main/java/ru/productstar/spring/jdbc1/manager/AccountDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface AccountDao {
     Account addAccount(long id, long amount);
 
+    Account addAccount(long amount);
+
     Account getAccount(long accountId);
 
     void setAmount(long accountId, long amount);
