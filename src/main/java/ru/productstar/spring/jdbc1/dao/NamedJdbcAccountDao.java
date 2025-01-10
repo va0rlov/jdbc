e3e152752984ff03a1d5c2d7fe111eb980bfa615
Виктор Орлов <vorlov@topsbi.ru>
2025-01-10 09:53:22 +0300

    'try-catch' added

--- src/main/java/ru/productstar/spring/jdbc1/dao/NamedJdbcAccountDao.java
+++ src/main/java/ru/productstar/spring/jdbc1/dao/NamedJdbcAccountDao.java
@@ -1,5 +1,7 @@
 package ru.productstar.spring.jdbc1.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -14,6 +16,7 @@ import java.util.List;
 public class NamedJdbcAccountDao implements AccountDao {
 
     private final NamedParameterJdbcTemplate namedJdbcTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(NamedJdbcAccountDao.class);
 
     public NamedJdbcAccountDao(NamedParameterJdbcTemplate namedJdbcTemplate) {
         this.namedJdbcTemplate = namedJdbcTemplate;
@@ -21,39 +24,48 @@ public class NamedJdbcAccountDao implements AccountDao {
 
     @Override
     public void addAccount(long id, long amount) {
-        namedJdbcTemplate.update("INSERT INTO ACCOUNT(ID, AMOUNT) VALUES(:id, :amount)", new MapSqlParameterSource().addValue("id", id).addValue("amount", amount)
-        );
+        try {
+            namedJdbcTemplate.update("INSERT INTO ACCOUNT(ID, AMOUNT) VALUES(:id, :amount)", new MapSqlParameterSource().addValue("id", id).addValue("amount", amount));
+        } catch (Exception e) {
+            logger.error("Ошибка при добавлении аккаунта с ID: {} и суммой: {}", id, amount, e);
+        }
     }
 
     @Override
     public Account getAccount(long accountId) {
-        return namedJdbcTemplate.queryForObject(
-                "SELECT ID, AMOUNT FROM ACCOUNT WHERE ID = :id",
-                new MapSqlParameterSource("id", accountId),
-                (rs, i) -> new Account(rs.getLong("ID"), rs.getLong("AMOUNT"))
-        );
+        try {
+            return namedJdbcTemplate.queryForObject("SELECT ID, AMOUNT FROM ACCOUNT WHERE ID = :id", new MapSqlParameterSource("id", accountId), (rs, i) -> new Account(rs.getLong("ID"), rs.getLong("AMOUNT")));
+        } catch (Exception e) {
+            logger.error("Ошибка при получении аккаунта с ID: {}", accountId, e);
+            return null; // Возвращаем null в случае ошибки
+        }
     }
 
     @Override
     public void setAmount(long accountId, long amount) {
-        namedJdbcTemplate.update(
-                "UPDATE ACCOUNT SET AMOUNT = :amount WHERE ID = :id",
-                new MapSqlParameterSource()
-                        .addValue("id", accountId)
-                        .addValue("amount", amount)
-        );
+        try {
+            namedJdbcTemplate.update("UPDATE ACCOUNT SET AMOUNT = :amount WHERE ID = :id", new MapSqlParameterSource().addValue("id", accountId).addValue("amount", amount));
+        } catch (Exception e) {
+            logger.error("Ошибка при обновлении суммы аккаунта с ID: {} на сумму: {}", accountId, amount, e);
+        }
     }
 
     @Override
     public List<Account> getAllAccounts() {
-        return namedJdbcTemplate.query(
-                "SELECT ID, AMOUNT FROM ACCOUNT",
-                (rs, i) -> new Account(rs.getLong("ID"), rs.getLong("AMOUNT"))
-        );
+        try {
+            return namedJdbcTemplate.query("SELECT ID, AMOUNT FROM ACCOUNT", (rs, i) -> new Account(rs.getLong("ID"), rs.getLong("AMOUNT")));
+        } catch (Exception e) {
+            logger.error("Ошибка при получении всех аккаунтов", e);
+            return Collections.emptyList(); // Возвращаем пустой список в случае ошибки
+        }
     }
 
     @Override
     public void deleteAllAccounts() {
-        namedJdbcTemplate.update("DELETE FROM ACCOUNT", Collections.emptyMap());
+        try {
+            namedJdbcTemplate.update("DELETE FROM ACCOUNT", Collections.emptyMap());
+        } catch (Exception e) {
+            logger.error("Ошибка при удалении всех аккаунтов", e);
+        }
     }
 }
